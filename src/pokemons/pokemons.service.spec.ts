49f8569418e42eb611f985b0128cf670939eeaa9
soportenoc <soportenoc@pormel.net>
2026-02-16 16:20:50 -0300

    'lol'

--- src/pokemons/pokemons.service.spec.ts
+++ src/pokemons/pokemons.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PokemonsService } from './pokemons.service';
+import { Result } from '../../dist/pokemons/interfaces/pokeapi.response';
+import { NotFoundException } from '@nestjs/common';
 
 describe('PokemonsService', () => {
   let service: PokemonsService;
@@ -39,4 +41,40 @@ describe('PokemonsService', () => {
         ]
       })
   })
+  it("shoul return 404 error if pokemon does't exist",async () => {
+
+    const id = 400_000;
+
+    await expect(service.findOne(id)).rejects.toThrow(NotFoundException);
+    await expect(service.findOne(id)).rejects.toThrow(`Pokemon with id ${id} not found`);
+  })
+
+  it('should chech properties of th pokemon',async () =>{
+    const id = 4;
+    const pokemon = await service.findOne(id);  
+
+    expect(pokemon).toHaveProperty('id');
+    expect(pokemon).toHaveProperty('name');
+    expect(pokemon).toEqual( expect.objectContaining({
+      id: id,
+      hp: expect.any(Number),
+
+    }));
+  })
+  it('should find all polemons and cache them', async () => {
+
+    const pokemons = await service.findAll({ limit: 10, page: 1});
+
+    expect(pokemons).toBeInstanceOf(Array);
+    expect(pokemons.length).toBe(10);
+
+    expect(service.paginatedPokemonsCache.has(
+    '10-1'
+    )).toBeTruthy();
+    expect(service.paginatedPokemonsCache.get(
+    '10-1'
+    )).toBe(pokemons);
+
+  })
+
 });
